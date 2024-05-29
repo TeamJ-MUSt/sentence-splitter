7194a0fa882eb499958a0af3127b38d1998e89d3
HappyFaceFriend <yjl980730@gmail.com>
2024-05-29 10:42:25 +0900

    Enhanced splitting algorithm

--- split.py
+++ split.py
@@ -1,5 +1,6 @@
 import argparse
 import fugashi
+from googletrans import Translator
 
 # This is our sample text.
 # "Fugashi" is a Japanese snack primarily made of gluten.
@@ -15,6 +16,11 @@ def parse_arguments():
 
     return args.query.strip()
 
+def translate_japanese_to_korean(sentence):
+    translator = Translator()
+    translation = translator.translate(sentence, src='ja', dest='ko')
+    return translation.text
+
 if __name__ == "__main__":
     query = parse_arguments()
 
@@ -22,11 +28,22 @@ if __name__ == "__main__":
 
     words = [word for word in tagger(query)]
 
+    splitter = ['助詞', '助動詞', '動詞'] #조사, 조동사
     chuncks = ['']
-    for word in words:
-        chuncks[-1] += word.surface
-        if '助' in word.feature.pos1 or '助' in word.feature.pos2:
-            if chuncks[-1] != '':
-                chuncks.append('')
+    slice_next = False
+    for i, word in enumerate(words):
+        if word.feature.pos1 in splitter:
+            if word.feature.pos1 == '動詞':
+                chuncks.append(word.surface)
+            else:
+                chuncks[-1] += word.surface
+            slice_next  =  True
+        else:
+            if slice_next:
+                chuncks.append(word.surface)
+                slice_next = False
+            else:
+                chuncks[-1] += word.surface
 
     print(chuncks)
+    #print(translate_japanese_to_korean(query))
